13c2c9baa5bfdc0bfd96c4960c1a3c56c408accc
Anna Muzalevskaya <77303782+NitaLewska@users.noreply.github.com>
2024-01-18 23:38:38 +0300

    fix: use path.join() and evade null in the console

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,8 +1,10 @@
 const fs = require('fs');
 const path = require('path');
-var absolutePath = path.resolve('./01-read-file/text.txt');
-var stream = new fs.ReadStream(absolutePath, { encoding: 'utf-8' });
+let absolutePath = path.join(path.resolve('/', __dirname), 'text.txt');
+let stream = new fs.ReadStream(absolutePath, { encoding: 'utf-8' });
 stream.on('readable', function () {
   var data = stream.read();
-  console.log(data);
+  if (data != null) {
+    console.log(data);
+  }
 });
