ab075dae8c2eaf88b9ee97c26a58210b0d9a8a5b
Anna Muzalevskaya <77303782+NitaLewska@users.noreply.github.com>
2024-01-22 03:32:10 +0300

    feat: implemented 05-merge-styles

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+fs.readdir(path.join(__dirname, '/secret-folder'), (err, files) => {
+  if (err) console.log(err);
+  else {
+    files.forEach((file) => {
+      if (file.isFile()) {
+        fs.stat(
+          path.join(__dirname, '/secret-folder', file.name),
+          (err, stats) => {
+            let [fileName, fileExtension] = [
+              file.name.split('.').slice(0, -1).join('.'),
+              file.name.split('.')[file.name.split('.').length - 1],
+            ];
+            console.log(`${fileName} - ${fileExtension} - ${stats.size} bytes`);
+          },
+        );
+      }
+    });
+  }
+});
---
+++ 05-merge-styles/index.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+let writeText = fs.createWriteStream(
+  path.join(__dirname, '/project-dist', 'bundle.css'),
+  {
+    flags: 'w',
+  },
+);
+fs.readdir(
+  path.join(__dirname, '/styles'),
+  { withFileTypes: true },
+  (err, files) => {
+    if (err) console.log(err);
+    else {
+      files.forEach((file) => {
+        if (file.name.endsWith('.css')) {
+          fs.readFile(
+            path.join(__dirname, '/styles', file.name),
+            (err, data) => {
+              if (err) throw err;
+              writeText.write(data.toString());
+              writeText.write('\n');
+            },
+          );
+        }
+      });
+    }
+  },
+);
